03aae3ce5913706328caa585a8ef77747b1ebbf3
kevin <kevin.voyer.developpeur@gmail.com>
2020-12-03 02:12:34 +0100

    Addjust bottom

--- src/components/User/chatSection/MessageContainer/ItemMapSearch/index.jsx
+++ src/components/User/chatSection/MessageContainer/ItemMapSearch/index.jsx
@@ -8,7 +8,9 @@ const useStyles = makeStyles((theme) => ({
   root: {
     height: '50%',
     width: 'calc(100% - 40px)',
-    padding: 20
+    paddingTop: 20,
+    paddingRight: 20,
+    marginBottom: 15,
   },
   title: {
     color: theme.palette.secondary.main,
@@ -33,39 +35,41 @@ const ItemMapSearch = ({
   };
 
   return (
-    <div className={classes.root}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: apiKey }}
-        defaultCenter={center}
-        defaultZoom={11}
-      >
-        {
-          items.map((item) => {
-            return (
-              <ItemMarker
-                address={item.address}
-                key={item.id}
-                icon={item.icon}
-                lat={item.lat}
-                lng={item.lng}
-                rate={item.rate}
-                name={item.name}
-                numberRate={item.numberRate}
-                internationalPhoneNumber={item.internationalPhoneNumber}
-                openNow={item.openNow}
-                openingWeekDays={item.openingWeekDays}
-                types={item.types}
-              />
-            );
-          })
-        }
-      </GoogleMapReact>
+    <>
+      <div className={classes.root}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: apiKey }}
+          defaultCenter={center}
+          defaultZoom={11}
+        >
+          {
+            items.map((item) => {
+              return (
+                <ItemMarker
+                  address={item.address}
+                  key={item.id}
+                  icon={item.icon}
+                  lat={item.lat}
+                  lng={item.lng}
+                  rate={item.rate}
+                  name={item.name}
+                  numberRate={item.numberRate}
+                  internationalPhoneNumber={item.internationalPhoneNumber}
+                  openNow={item.openNow}
+                  openingWeekDays={item.openingWeekDays}
+                  types={item.types}
+                />
+              );
+            })
+          }
+        </GoogleMapReact>
+      </div>
       <MessageDescription
         by={by}
         date={date}
         from={from}
       />
-    </div>
+    </>
   );
 };
 
